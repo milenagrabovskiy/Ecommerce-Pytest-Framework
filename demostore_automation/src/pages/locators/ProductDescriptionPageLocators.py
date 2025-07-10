6b6c8aea5ea696af55725cc60113e4cee9c9c3ec
Milena Grabovskiy <mile15999@gmail.com>
2025-07-09 20:48:53 -0400

    fixed pdp locator file

--- demostore_automation/src/pages/locators/ProductDescriptionPageLocators.py
+++ demostore_automation/src/pages/locators/ProductDescriptionPageLocators.py
@@ -4,8 +4,5 @@ from selenium.webdriver.common.by import By
 class ProductDescriptionPageLocators:
 
     PRODUCT_TITLE = (By.CSS_SELECTOR, "h1.product_title.entry-title")
-    #PRODUCT_MAIN_IMAGE = (By.CSS_SELECTOR, 'img[src="http://dev.bootcamp.store.supersqa.com/wp-content/uploads/2024/10/hoodie-2.jpg"]')
-    #PRODUCT_MAIN_IMAGE = (By.CLASS_NAME, 'zoomImg')
     PRODUCT_MAIN_IMAGE = (By.CSS_SELECTOR, 'img.wp-post-image')
-    #PRODUCT_MAIN_IMAGE = (By.XPATH, '//*[@id="product-20"]/div[1]/div/div/div[1]/img')
     PRODUCT_ALTERNATE_IMAGES = (By.CSS_SELECTOR, 'ol.flex-control-nav.flex-control-thumbs li img')
